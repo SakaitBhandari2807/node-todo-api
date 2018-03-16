6d20a6a3cd719af46bd8cc81747be05531709730
SakaitBhandari2807 <sakaitbhandari@outlook.com>
2018-03-16 17:40:46 +0530

    Add Post todo route and refactor routes

--- server/server.js
+++ server/server.js
@@ -1,63 +1,25 @@
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js')
 
-// const express = require('express');
-// Adding models
-// var Todo = mongoose.model('Todo',{
-//     text:{
-//         type:String,
-//         required:true,
-//         minlength:1,
-//         trim:true
-//     },
-//     completed:{
-//         type:Boolean,
-//         default:false
-//     },
-//     completedAt:{
-//         type:Number,
-//         default:null
-//     }
-// }); 
+var app = express();
 
-var User = mongoose.model('Users',{
-    email:{
-        type:String,
-        required:true,
-        minlength:1,
-        trim:true
-    }
-});
+//Middleware
+app.use(bodyParser.json());
 
-var newUser = new User({
-  email:'souravandsakait@gmail.com'    
+app.post('/todos',(req,res)=>{
+   var todo = new Todo({
+       text:req.body.text
+   })
+   todo.save().then((doc)=>{
+     res.send(doc);
+   },(err)=>{
+       res.status(400).send(err);
+   })
 })
-
-newUser.save().then((docs)=>{
-  console.log(JSON.stringify(docs,undefined,2));
-},(err)=>{
-    console.log(`Unable to save User`,err);
+app.listen(3000,()=>{
+    console.log(`Started on port 3000`);
 })
-// var newTodo = new Todo({ 
-//     text:'Cook dinner',
-// })
-
-// newTodo.save().then((doc)=>{
-//   console.log('Saved todo',doc);
-// },(err)=>{
-//     console.log('Unable to save todo',err);
-// });
-
-// var otherTodo = new Todo({
-//     text:'Switch companies',
-//     completed:false,
-//     completedAt:124
-// });
-
-// otherTodo.save().then((docs)=>{
-//   console.log(JSON.stringify(docs,undefined,2));
-// },(err)=>{
-//    console.log(`Unable to save to Todo collection`);
-// });
